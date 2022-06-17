f5ff2ae913f996c7cba7da9287cae659a6a09e45
Phu Nguyen <ngp130895@gmail.com>
2022-06-17 12:26:50 +0300

    feat: added s3 and code pipeline

--- lib/techtalk-cdk-test-stack.ts
+++ lib/techtalk-cdk-test-stack.ts
@@ -1,6 +1,8 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps,  } from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
 
 export class TechtalkCdkTestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -12,5 +14,16 @@ export class TechtalkCdkTestStack extends Stack {
     // const queue = new sqs.Queue(this, 'TechtalkCdkTestQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // create an S3 bucket
+    const bucket = new Bucket(this, 'TechtalkCdkTestBucket');
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('phuwin95/hackday-cdk2-lambda-dynamodb', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+      }),
+    });
   }
 }
