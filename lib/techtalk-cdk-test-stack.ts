ea91e1f650edd90c6d26312b538a5f06a2606051
Phu Nguyen <ngp130895@gmail.com>
2022-06-17 12:39:36 +0300

    feat: added lambda

--- lib/techtalk-cdk-test-stack.ts
+++ lib/techtalk-cdk-test-stack.ts
@@ -1,8 +1,11 @@
-import { Stack, StackProps,  } from 'aws-cdk-lib';
+import { Duration, Stack, StackProps, Stage, StageProps,  } from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Table, AttributeType } from 'aws-cdk-lib/aws-dynamodb';
+import { join } from 'path';
 
 export class TechtalkCdkTestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -21,9 +24,34 @@ export class TechtalkCdkTestStack extends Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('phuwin95/hackday-cdk2-lambda-dynamodb', 'main'),
+        input: CodePipelineSource.gitHub('phuwin95/techtalk-cdk-test', 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth'],
       }),
     });
+
+    const lambdaStage = new MyStage(this, 'LambdaStage');
+  }
+}
+
+class MyStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const myFunction = new NodejsFunction(this, 'my-function', {
+      memorySize: 256,
+      timeout: Duration.seconds(5),
+      runtime: Runtime.NODEJS_14_X,
+      handler: 'main',
+      entry: join(__dirname, `/../src/my-lambda/index.ts`),
+    });
+  }
+}
+
+class MyStage extends Stage {
+    
+  constructor(scope: Construct, id: string, props?: StageProps) {
+    super(scope, id, props);
+
+    const stack = new MyStack(this, 'LambdaDynamodbStack');      
   }
 }
